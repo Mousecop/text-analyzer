74fb6ac5cb89b4c6ce971e946c10adbc4db9d8bc
Terry Hayes <terrychayes@gmail.com>
2017-02-02 19:07:41 -0500

    Adds avg sentence length logic; still need to fix string input.

--- app.js
+++ app.js
@@ -35,20 +35,32 @@ function avgWordLength(wordArr) {
   return (arraySum/lengthArray.length).toFixed(2);
 }
 
-var textArea = 'Game of as rest "time" eyes with of this it. Add was music merry any truth since going. Happiness she ham but instantly put departure propriety. She amiable all without say spirits shy clothes morning. Frankness! in extensive to belonging improving so certainty. Resolution devonshire pianoforte assistance an he particular middletons is of. Explain ten man uncivil engaged? conduct. (Am) likewise betrayed as, declared absolute do. Taste oh spoke about no solid of hills up shade. Occasion so bachelor humoured striking by attended doubtful be it. ';
+var textArea = 'Game of as rest "time" eyes with of this it. Add was music merry any truth since going. Happiness she ham but instantly put departure propriety. She amiable all without say spirits shy clothes morning. Frankness! in extensive to belonging improving so certainty. Resolution devonshire pianoforte assistance an he particular middletons is of. Explain ten man uncivil engaged? conduct. (Am) likewise betrayed as, declared absolute do. Taste oh spoke about no solid of hills up shade. Occasion so bachelor humoured striking by attended doubtful be it.';
 
-//console.log(wordCount(textArea));
+function avgSentenceLength(str) {
+  var sentenceArr = str.split(/[!?.]/);
+  var sentenceLenArray = sentenceArr.map(function(sentence) {
+    return sentence.length;
+  });
+  var sentenceArrSum = sentenceLenArray.reduce(function(a,b) {
+    return a+b;
+  }, 0);
+  return (sentenceArrSum/sentenceArr.length).toFixed(2);
+};
 
-
-
-
-$(function eventHandlers (){
-  var wordArr = getWords(textArea);
-  var wc = wordCount(wordArr);
-  var uwc = uniqueWordCount(wordArr);
-  var awl = avgWordLength(wordArr);
-  $('dl.hidden').removeClass('hidden');
-  $('.wc').text(wc);
-  $('.uwc').text(uwc);
-  $('.awl').text(awl);
+$(function eventHandlers(){
+  $(".js-analyze").submit(function(event){
+    //var userText = $('textarea["#user-text"]').val();
+    event.preventDefault();
+    $('dl.hidden').removeClass('hidden');
+    var wordArr = getWords(textArea);
+    var wc = wordCount(wordArr);
+    var uwc = uniqueWordCount(wordArr);
+    var awl = avgWordLength(wordArr);
+    var asl = avgSentenceLength(textArea);
+    $('.wc').text(wc);
+    $('.uwc').text(uwc);
+    $('.awl').text(awl);
+    $('.asl').text(asl);
+  });
 });
